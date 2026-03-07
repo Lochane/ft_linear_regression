535dd2df0e785e1d102ea3fa982b2ff9b3c7e4f3
Bugs <lochane22@gmail.com>
2026-03-07 14:40:28 +0100

    Update readme

--- src/persistence/model_io.py
+++ src/persistence/model_io.py
@@ -2,11 +2,11 @@ import json
 import os
 
 def save_model(params: dict, filepath :str):
-	"""Saves the model to a specified path in JSON format.
+	"""Creates the directory for the model file and saves the model to a specified path in JSON format.
 
 	Args:
 		params: The model parameters to be saved.
-		path: The path where the model will be saved.
+		filepath: The path where the model will be saved.
 	"""
 	os.makedirs(os.path.dirname(filepath), exist_ok=True)
 	print(f"Directory '{filepath}' created successfully.")
@@ -14,11 +14,11 @@ def save_model(params: dict, filepath :str):
 		json.dump(params, f)
 		print(f"\033[32mModel parameters saved to {filepath}\033[0m")
 
-def load_model(path: str) -> dict:
+def load_model(filepath: str) -> dict:
 	"""Loads the model from a specified path in JSON format.
 
 	Args:
-		path: The path from which the model will be loaded.
+		filepath: The path from which the model will be loaded.
 
 	Returns:
 		The loaded model parameters as a dictionary.
@@ -29,9 +29,9 @@ def load_model(path: str) -> dict:
 		Exception: For any other unexpected errors.
 	"""
 	try:
-		with open(path, "r") as file:
+		with open(filepath, "r") as file:
 			params = json.load(file)
-			# print(f"\033[32mModel loaded successfully from {path}\033[0m")
+			# print(f"\033[32mModel loaded successfully from {filepath}\033[0m")
 			return params
 	except FileNotFoundError:
 		raise 
